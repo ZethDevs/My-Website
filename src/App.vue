fa2fc17be8f15a2d254694f2ea279e7ab976b538
Lutfi Farid <lutfi.faridd@gmail.com>
2023-07-12 16:02:30 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@
     <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
     <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
     <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
-    <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
+  <!--  <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link> -->
   </nav>
 </footer>
 </template>
